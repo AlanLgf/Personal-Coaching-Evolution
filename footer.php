87bfaf841a0cf97b0a054b6d6e9e4df32290b19e
Neokoiaigone <louisalexandrebricq@yahoo.fr>
2017-05-03 11:59:14 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+    <title>Footer</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css">
+    <link href="style3.css" rel="stylesheet">
+</head>
+
+<body>
+
+<!-- Div contact animation bouton & input -->
+
+<!-- Footer -->
+<footer class="wow fadeIn navbar-default footer wow FadeInDown">
+    <div class="footer-above background-footer-color">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col col-md-12">
+                    <div class="text-center">
+                        <ul class="list-inline text-center">
+                            <li class="text">Nous contacter
+                                <p class="margintop">Email</p>
+                                <p>Tel</p>
+                            </li>
+                            <li class="text navbar-right">  
+                                Nos réseaux sociaux
+                                <p class="margintop"><a href="#" class="brightness"><img src="img/fb_logo.png" alt="facebook" width="50" height="50"></a>
+                                <a href="#" class="brightness"><img src="img/Twitterbird.png" alt="twitter" width="50" height="50"></a></p>
+                            </li>
+                            <li class="text navbar-left">  
+                                About us ! 
+                                <p class="margintop"><a href="#">blablabla</a></p>
+                                <p><a href="#">blablabla</a></p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
