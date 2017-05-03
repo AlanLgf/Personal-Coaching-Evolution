c906cb733fd4cc08c7bd0243cafbc2bef2f11555
Alanlgf <alanlegoff78@gmail.com>
2017-05-03 15:03:15 +0200

    Fin de la mise en forme page Accueil

--- index.php
+++ index.php
@@ -11,13 +11,41 @@ get_header();
  		<h1>Un super texte</h1>
  	</div>
 
- 	<div class="container">
- 		<div class="row">
- 			<div class="col-md-4 col-sm-4 col-xs-12">
- 				
- 			</div>
- 		</div>
- 	</div>
+	<div id="objectifs">
+	 	<div class="container">
+			<div class="row">
+				<h2 class="center">Objectifs</h2>
+				<div class=" col-md-4 col-sm-4 col-xs-12 center">
+					<a href="#" target="_blank"><img src="http://www.optionfitness.fr/img/picture5.jpg"></a>
+					<p>Sollicita ratione omnibus hominum usu tyrannorum fiducia Haec cognitam sollicita cognitam deorum hominum atque fides ut potest quando atque fides.</p>
+				</div>
+				<div class=" col-md-4 col-sm-4 col-xs-12 center">
+					<a href="#" target="_blank"><img src="http://www.optionfitness.fr/img/picture5.jpg"></a>
+					<p>Sollicita ratione omnibus hominum usu tyrannorum fiducia Haec cognitam sollicita cognitam deorum hominum atque fides ut potest quando atque fides.</p>
+				</div>
+				<div class=" col-md-4 col-sm-4 col-xs-12 center">
+					<a href="#" target="_blank"><img src="http://www.optionfitness.fr/img/picture5.jpg"></a>
+					<p>Sollicita ratione omnibus hominum usu tyrannorum fiducia Haec cognitam sollicita cognitam deorum hominum atque fides ut potest quando atque fides.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="offres">
+	 	<div class="container">
+			<div class="row">
+				<h2 class="center">Découvrez nos offres</h2>
+				<div class=" col-md-6 col-sm-6 col-xs-12 center">
+					<a href="#" target="_blank"><img src="http://www.optionfitness.fr/img/picture5.jpg"></a>
+				</div>
+				<div class=" col-md-6 col-sm-6 col-xs-12 center">
+					<a href="#" target="_blank"><img src="http://www.optionfitness.fr/img/picture5.jpg"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+
 
 
 
