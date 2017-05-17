b09d90c6730d1dea2d76548fed2f64b5229fbc69
Alanlgf <alanlegoff78@gmail.com>
2017-05-17 16:23:37 +0200

    Ajout de la page exercice

---
+++ exercice.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Template Name: Exercice
+ */
+?>
+<?php
+get_header();
+?>
+
+<div id="exercice">
+
+<div id="courseapied">
+
+	<h1>Exercice</h1>
+	<h2>Course a pied</h2>
+
+	<div class="container" >
+		<div class="row">
+			<div class=" col-md-6 col-sm-6 col-xs-12 parcours">
+				<div class="mataille">
+					<img src="<?php bloginfo('url'); ?>/wp-content/themes/Personal Coaching Evolution/img/Course-à-pieds.jpg">
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="alignement">
+				<p class="montexte">Sed ut tum ad senem senex de senectute, sic hoc libro ad amicum amicissimus scripsi de amicitia. Tum est Cato locutus, quo erat nemo fere senior temporibus illis, nemo prudentior; nunc Laelius et sapiens (sic enim est habitus) et amicitiae gloria excellens de amicitia loquetur. Tu velim a me animum parumper avertas, Laelium loqui ipsum putes. C. Fannius et Q. Mucius ad socerum veniunt post mortem Africani; ab his sermo oritur, respondet Laelius, cuius tota disputatio est de amicitia, quam legens te ipse cognosces.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+<div id="Pilates">
+<hr class="styleexercice">
+	<h2>Pilates</h2>
+	<div class="container" >
+		<div class="row">
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="alignement">
+				<p class="montexte">Sed ut tum ad senem senex de senectute, sic hoc libro ad amicum amicissimus scripsi de amicitia. Tum est Cato locutus, quo erat nemo fere senior temporibus illis, nemo prudentior; nunc Laelius et sapiens (sic enim est habitus) et amicitiae gloria excellens de amicitia loquetur. Tu velim a me animum parumper avertas, Laelium loqui ipsum putes. C. Fannius et Q. Mucius ad socerum veniunt post mortem Africani; ab his sermo oritur, respondet Laelius, cuius tota disputatio est de amicitia, quam legens te ipse cognosces.</p>
+				</div>
+			</div>
+			<div class=" col-md-6 col-sm-6 col-xs-12 parcours">
+				<div class="mataille">
+					<img src="<?php bloginfo('url'); ?>/wp-content/themes/Personal Coaching Evolution/img/pilates-fitness.jpg">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<hr class="styleexercice">
+
+<div id="TRX">
+	<h2>TRX</h2>
+
+	<div class="container" >
+		<div class="row">
+			<div class=" col-md-6 col-sm-6 col-xs-12 parcours">
+				<div class="mataille">
+					<img src="<?php bloginfo('url'); ?>/wp-content/themes/Personal Coaching Evolution/img/trx.jpg">
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="alignement">
+				<p class="montexte">Sed ut tum ad senem senex de senectute, sic hoc libro ad amicum amicissimus scripsi de amicitia. Tum est Cato locutus, quo erat nemo fere senior temporibus illis, nemo prudentior; nunc Laelius et sapiens (sic enim est habitus) et amicitiae gloria excellens de amicitia loquetur. Tu velim a me animum parumper avertas, Laelium loqui ipsum putes. C. Fannius et Q. Mucius ad socerum veniunt post mortem Africani; ab his sermo oritur, respondet Laelius, cuius tota disputatio est de amicitia, quam legens te ipse cognosces.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+<hr class="styleexercice">
+
+<div id="coursenentreprise">
+	<h2>Cours en entreprise</h2>
+	<div class="container" >
+		<div class="row">
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="alignement">
+				<p class="montexte">Sed ut tum ad senem senex de senectute, sic hoc libro ad amicum amicissimus scripsi de amicitia. Tum est Cato locutus, quo erat nemo fere senior temporibus illis, nemo prudentior; nunc Laelius et sapiens (sic enim est habitus) et amicitiae gloria excellens de amicitia loquetur. Tu velim a me animum parumper avertas, Laelium loqui ipsum putes. C. Fannius et Q. Mucius ad socerum veniunt post mortem Africani; ab his sermo oritur, respondet Laelius, cuius tota disputatio est de amicitia, quam legens te ipse cognosces.</p>
+				</div>
+			</div>
+			<div class=" col-md-6 col-sm-6 col-xs-12 parcours">
+				<div class="mataille">
+					<img src="<?php bloginfo('url'); ?>/wp-content/themes/Personal Coaching Evolution/img/Coursenentreprise.png">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<hr class="styleexercice">
+
+<div id="crossfit-crosstraining">
+	<h2>Crossfit / Crosstraining</h2>
+
+	<div class="container" >
+		<div class="row">
+			<div class=" col-md-6 col-sm-6 col-xs-12 parcours">
+				<div class="mataille">
+					<img src="<?php bloginfo('url'); ?>/wp-content/themes/Personal Coaching Evolution/img/crossfit.jpg">
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="alignement">
+				<p class="montexte">Sed ut tum ad senem senex de senectute, sic hoc libro ad amicum amicissimus scripsi de amicitia. Tum est Cato locutus, quo erat nemo fere senior temporibus illis, nemo prudentior; nunc Laelius et sapiens (sic enim est habitus) et amicitiae gloria excellens de amicitia loquetur. Tu velim a me animum parumper avertas, Laelium loqui ipsum putes. C. Fannius et Q. Mucius ad socerum veniunt post mortem Africani; ab his sermo oritur, respondet Laelius, cuius tota disputatio est de amicitia, quam legens te ipse cognosces.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+</div>
+
+<?php
+get_footer();
+?>
