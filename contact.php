6f70c5c3aa6efce85327545ddaa1bd794b9bdd62
Alanlgf <alanlegoff78@gmail.com>
2017-05-30 16:26:06 +0200

    J'ai pas d'idée pour le contact

--- contact.php
+++ contact.php
@@ -10,13 +10,11 @@ get_header();
     
 <div id="contact">
 	<div class="container">
-		<div class="row">
 			<div class="col-md-12">
                 <?php
 				echo do_shortcode('[contact-form-7 id="68" title="Formulaire de contact 1"]');
 		?>
 			</div>
-		</div>
 	</div>
 </div>
 
